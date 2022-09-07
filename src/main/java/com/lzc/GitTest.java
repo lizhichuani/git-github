777e8cff080b00e313f71ff294b976e3352a6730
lzc <1474609635@qq.com>
2022-09-07 22:35:04 +0800

    push test commit

--- src/main/java/com/lzc/GitTest.java
+++ src/main/java/com/lzc/GitTest.java
@@ -1,11 +1,11 @@
 package com.lzc;
 
 public class GitTest {
-
     public static void main(String[] args) {
         System.out.println("hello git!");
         System.out.println("hello git2");
         System.out.println("hello git3");
+        System.out.println("push test");
     }
 
 }
