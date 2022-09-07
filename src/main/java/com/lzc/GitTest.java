a9696d98b1938778ac99364441091b9722570cd9
lzc <1474609635@qq.com>
2022-09-07 22:49:51 +0800

    push ssh test commit

--- src/main/java/com/lzc/GitTest.java
+++ src/main/java/com/lzc/GitTest.java
@@ -7,6 +7,7 @@ public class GitTest {
         System.out.println("hello git3");
         System.out.println("push test");
         System.out.println("push test2");
+        System.out.println("push ssh test");
     }
 
 }
